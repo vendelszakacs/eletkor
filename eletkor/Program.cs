6558775005b2de27269ced7e1604001cd044b347
Petrik Diak <diak@petrik.hu>
2026-01-21 12:17:00 +0100

    rfvrvrv

--- eletkor/Program.cs
+++ eletkor/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("1. feladat megoldása:");
+
+            for (int age = 10; age <= 99; age++)
+            {
+                int tizes = age / 10;
+                int egyes = age % 10;
+
+                int swapped = egyes * 10 + tizes;
+
+                if (swapped == 2 * (age + 1))
+                {
+                    Console.WriteLine($"A testvér mostani életkora: {age} év");
+                }
+            }
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("2. feladat: (Ha felcserélném anyukám jelenlegi életkorának (két számjegyű életkor) számjegyeit, akkor három évvel ezelőtti korának háromszorosát kapnám. Hány éves lehet most?) megoldása:");
+
+            for (int age = 10; age <= 99; age++)
+            {
+                int tizes = age / 10;
+                int egyes = age % 10;
+
+                int swapped = egyes * 10 + tizes;
+
+                if (swapped == 3 * (age - 3))
+                {
+                    Console.WriteLine($"Anyukám mostani életkora: {age} év");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("A megoldások keresése befejeződött. Nyomj egy Entert a kilépéshez.");
+            Console.ReadLine();
         }
     }
 }
